ee3befc8e6e8eb0587d0bd3899bd9913c9047eb3
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 11:19:13 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);        int a,b;        a=sc.nextInt();        b=sc.nextInt();
          int res=a%b;
            System.out.println(res);
            
        }
    }

---
+++ Modulus.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+       int a,b;
+       a=sc.nextInt();
+       b=sc.nextInt();
+        
+      int res=a%b;
+        System.out.println(res);
+        
+    }
+}
