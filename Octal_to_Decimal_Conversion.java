c9d7cf27bdb7deddbe059de5cbc4ecd2df8780d2
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 10:41:02 +0530

    import java.util.*; public class A{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String D=sc.next();         int f=Integer.parseInt(D,8);         System.out.println(f);
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class A{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String D=sc.next();
+        int f=Integer.parseInt(D,8);
+        System.out.println(f);
+    
+        
+    }
+}
