1dfcc5e27e23e1a14f6f0d02f2b94616a161c5f5
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 10:56:14 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int t,s;         t=sc.nextInt();         s=sc.nextInt();
            int result=t+s;
            System.out.println(result);
            
        }
    }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t,s;
+        t=sc.nextInt();
+        s=sc.nextInt();
+        
+        int result=t+s;
+        System.out.println(result);
+        
+    }
+}
