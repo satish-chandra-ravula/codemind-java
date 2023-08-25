a05b4e66b12ae01252956b1daf2544e513e4c7f9
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 11:09:02 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();
            int result=(n*(n-1))/2;
            System.out.println(result);
            
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        
+        int result=(n*(n-1))/2;
+        System.out.println(result);
+        
+    }
+}
