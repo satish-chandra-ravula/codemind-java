b170c0ebeae0179d8c44a34f3c9824d8d6be4e77
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 11:33:27 +0530

    import java.util.*; public class M{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);        double a,b;        a=sc.nextInt();        b=sc.nextInt();         double res=(a+b)/2;         System.out.printf("%.4f",res);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+       double a,b;
+       a=sc.nextInt();
+       b=sc.nextInt();
+        double res=(a+b)/2;
+        System.out.printf("%.4f",res);
+        
+    }
+}
