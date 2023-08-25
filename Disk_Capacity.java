5924e4a34803cdb2ee21e142403fba5332a127a4
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-25 10:48:03 +0530

    import java.util.*; public class A{     public static void main(String[] args){     Scanner m=new Scanner(System.in);     int t,s,b;     t=m.nextInt();     s=m.nextInt();     b=m.nextInt();     int result=t*s*b*512*2;     System.out.println(result);
        }
    }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class A{
+    public static void main(String[] args){
+    Scanner m=new Scanner(System.in);
+    int t,s,b;
+    t=m.nextInt();
+    s=m.nextInt();
+    b=m.nextInt();
+    int result=t*s*b*512*2;
+    System.out.println(result);
+        
+    }
+}
