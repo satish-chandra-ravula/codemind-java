ab129995244557bfc418b6b3a146faf584d0eb12
satish-chandra-ravula <119839447+satish-chandra-ravula@users.noreply.github.com>
2023-08-29 10:44:15 +0530

    import java.util.*; public class M{     public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     double cs =sc.nextInt();     double F=32+(cs*1.8);     System.out.printf("%.2f",F);     }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class M{
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    double cs =sc.nextInt();
+    double F=32+(cs*1.8);
+    System.out.printf("%.2f",F);
+    }
+    
+}
